041380c3a1775b3c724ca650679ecb97bb21568b
Ali <aeslami111@gmail.com>
2020-10-15 10:25:59 +0200

    revision on regex,comments,output print,

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -16,7 +16,7 @@ def get_next_token(text):
         if type(tokens_dfa[state]) is dict and Position.index < len(text):
             # matched = False
             for s in tokens_dfa[state].keys():
-                # print(text[Position.index], s)
+                #print(text[Position.index], s)
                 if re.search(s, text[Position.index]):
                     # matched = True
                     state = tokens_dfa[state][s]
@@ -38,7 +38,7 @@ def get_next_token(text):
         else:
             if state in stars_states:
                 Position.index -= 1
-            elif Position.index >= len(text):
+            elif Position.index > len(text):
                 state = list(tokens_dfa[state].values())[-1]
             return (tokens_dfa[state], text[first_index: Position.index]), line
 
@@ -62,13 +62,13 @@ def get_next_token(text):
 
 class Position:
     index = 0
-    line = 0
+    line = 1
 
     @staticmethod
     def advance(text):
         Position.index += 1
         # print(Position.index, Position.line)
-        if Position.index < len(text) and text[Position.index] == '\n':
+        if Position.index < len(text) and text[Position.index-1] == '\n':
             Position.line += 1
 
 
@@ -95,12 +95,17 @@ class Lexer:
 
         while Position.index < len(self.text):
             (token, line) = get_next_token(self.text)
-            # print(token)
+            #print(token)
+            if token[0] == "WHITESPACE":
+                continue
+            #print(token,line)
             if line in tokens:
                 tokens[line].append(token)
             else:
                 tokens[line] = [token]
-        print(tokens)
+        #print(tokens)
+        for row in tokens:
+            print(row,"-",tokens[row])
         # print(Position.index)
 
             # self.advance()
