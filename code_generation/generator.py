7d5bcd5f800310021a7dc4b06bbefbecdc1fd959
Ali <aeslami111@gmail.com>
2020-12-20 13:14:46 +0330

    start_codegen

--- code_generation/generator.py
+++ code_generation/generator.py
@@ -1,6 +1,40 @@
+semantic_stack = []
+program_block = []
+symbol_table = {}
+temp_index = 1000
+symbol_index = 500
 
 
-def code_gen(action):
+def findAddr(symbol):
+    for s in symbol_table:
+        if s[0] == symbol:
+            return s[1]
+
+
+def getTemp():
     pass
+
+def generate_code(action):
+    if action == "#pid":
+        print()
+        p = findAddr(input)
+        semantic_stack.append(p)
+
+    elif action == "#add":
+        t = getTemp()
+
+        print()
+
+
+    elif action == "#mult":
+        print()
+
+    elif action == "#assign":
+        print()
+
+    elif action == "#add":
+        print()
+
+
 
 
